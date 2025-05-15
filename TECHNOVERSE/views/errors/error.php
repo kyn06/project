0cdcd4fd712c275f123a4b762a8092b03db96f56
Jhe <kwinjherane.ocareza@clsu2.edu.ph>
2025-05-15 21:01:35 +0800

    Update error.php
    Co-Authored-By: drixpogi <199545249+drixpogi@users.noreply.github.com>
    Co-Authored-By: Little Vinz is stressed out <211037746+inzt18@users.noreply.github.com>

--- TECHNOVERSE/views/errors/error.php
+++ TECHNOVERSE/views/errors/error.php
@@ -1,4 +1,4 @@
 <?php
 // any PHP logic here if needed
 ?>
-<img src="error/error4.png" alt="Error Image" class="img-fluid mb-4" style="max-width: 300px;">
+<img src="../../public/assets/images/error.jpg" alt="Error Image" class="img-fluid mb-4" style="max-width: 300px;">
